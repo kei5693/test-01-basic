2611c0e74c791c784babdd0d0d16ac09e947a88f
hdk236p <keichi5693@gmail.com>
2023-10-03 23:25:06 +0900

    8.9까지 학습

---
+++ chapter_08/src/components/TodoList/TodoList.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Todo from '../Todo/Todo';
+import AddTodo from '../AddTodo/AddTodo';
+
+export default function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: '123', text: '장보기', status: 'active' },
+    { id: '124', text: '공부하기', status: 'active' },
+  ]);
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) => setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) => setTodos(todos.filter((t) => t.id !== deleted.id));
+
+  return (
+    <section>
+      <ul>
+        {todos.map((item) => (
+          <Todo
+						key={item.id}
+						todo={item}
+						onUpdate={handleUpdate}
+						onDelete={handleDelete}
+					/>
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
+}
